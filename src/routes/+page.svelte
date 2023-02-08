525e1207904e957d5bef6dfe8fa704ecb3123982
petrzamecnik <petazamecek@gmail.com>
2023-02-08 01:20:22 +0100

    init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import '../global.scss'
+</script>
+
+<div class="center">
+  <h1>Hello svelte mobile world!</h1>
+</div>
